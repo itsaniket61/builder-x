1939012adb9322ce373b8eb099c4604fb694fe67
Aniket Sharma <aniket.sharma.4509@gmail.com>
2024-04-14 12:40:37 +0530

    Download on Click

--- components/Explorer/FileList.jsx
+++ components/Explorer/FileList.jsx
@@ -35,7 +35,6 @@ const FileList = ({ folderPath, files, selectFolder }) => {
   const [currentSelectedFile, setCurrentSelectedFile] = useState(undefined);
 
   return (
-    <ContextMenu>
       <Table>
         <TableHeader>
           <TableRow>
@@ -61,7 +60,10 @@ const FileList = ({ folderPath, files, selectFolder }) => {
                     selectFolder(file.name);
                   } else {
                     setCurrentSelectedFile(file);
-                    console.log(currentSelectedFile);
+                    const { downloadUrl } = await explorerUtil.downloadFile({
+                      filePath: folderPath + '/' + currentSelectedFile.name,
+                    });
+                    window.open(downloadUrl, '_blank');
                   }
                 }}
               >
@@ -82,61 +84,6 @@ const FileList = ({ folderPath, files, selectFolder }) => {
           })}
         </TableBody>
       </Table>
-
-      <ContextMenuTrigger>
-        
-      </ContextMenuTrigger>
-      <ContextMenuContent className='w-64'>
-        <ContextMenuItem
-          inset
-          onClick={async () => {
-            const { downloadUrl } = await explorerUtil.downloadFile({
-              filePath: folderPath + '/' + currentSelectedFile.name,
-            });
-            window.open(downloadUrl, '_blank');
-          }}
-        >
-          Download
-          <ContextMenuShortcut>⌘[</ContextMenuShortcut>
-        </ContextMenuItem>
-        <ContextMenuItem inset disabled>
-          Forward
-          <ContextMenuShortcut>⌘]</ContextMenuShortcut>
-        </ContextMenuItem>
-        <ContextMenuItem inset>
-          Reload
-          <ContextMenuShortcut>⌘R</ContextMenuShortcut>
-        </ContextMenuItem>
-        <ContextMenuSub>
-          <ContextMenuSubTrigger inset>More Tools</ContextMenuSubTrigger>
-          <ContextMenuSubContent className='w-48'>
-            <ContextMenuItem>
-              Save Page As...
-              <ContextMenuShortcut>⇧⌘S</ContextMenuShortcut>
-            </ContextMenuItem>
-            <ContextMenuItem>Create Shortcut...</ContextMenuItem>
-            <ContextMenuItem>Name Window...</ContextMenuItem>
-            <ContextMenuSeparator />
-            <ContextMenuItem>Developer Tools</ContextMenuItem>
-          </ContextMenuSubContent>
-        </ContextMenuSub>
-        <ContextMenuSeparator />
-        <ContextMenuCheckboxItem checked>
-          Show Bookmarks Bar
-          <ContextMenuShortcut>⌘⇧B</ContextMenuShortcut>
-        </ContextMenuCheckboxItem>
-        <ContextMenuCheckboxItem>Show Full URLs</ContextMenuCheckboxItem>
-        <ContextMenuSeparator />
-        <ContextMenuRadioGroup value='pedro'>
-          <ContextMenuLabel inset>People</ContextMenuLabel>
-          <ContextMenuSeparator />
-          <ContextMenuRadioItem value='pedro'>
-            Pedro Duarte
-          </ContextMenuRadioItem>
-          <ContextMenuRadioItem value='colm'>Colm Tuite</ContextMenuRadioItem>
-        </ContextMenuRadioGroup>
-      </ContextMenuContent>
-    </ContextMenu>
   );
 };
 
